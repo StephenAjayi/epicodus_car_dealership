61ff6edc04a8dec8238588a76d5f95af27b608f1
Paul Ogawa & stephen <student@epicodus.com>
2015-04-29 11:31:20 -0700

    it adds tests for clearing the array and finding by id all passing test

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -5,6 +5,7 @@ class Dealership
   define_method(:initialize) do |name|
     @name = name
     @dealer_id = @@dealerships.length+1
+    @cars = []
   end
 
   define_method(:name) do
@@ -20,8 +21,25 @@ class Dealership
   end
 
 
+  def self.all
+    @@dealerships
+  end
 
+  def cars
+    @cars
+  end
 
+  def self.clear
+    @@dealerships = []
+  end
 
-
+  define_singleton_method(:find) do |dealer_id|
+    found_dealership = nil
+    @@dealerships.each() do |dealership|
+      if dealership.dealer_id().eql?(dealer_id)
+        found_dealership = dealership
+      end
+    end
+    found_dealership
+  end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -4,6 +4,9 @@ require "pry"
 
 
 describe(Dealership) do
+  before() do
+      Dealership.clear()
+    end
 
   describe('#name') do
     it("will create an instance of dealership that has a name") do
@@ -19,13 +22,52 @@ describe(Dealership) do
     end
   end
 
-  desscribe('#save') do
+  describe('#save') do
     it('saves the object and adds it to an array') do
       test_dealership = Dealership.new('bobs dealership')
       test_dealership.save
-      expect(test_dealership.save()).to(eq([test_dealership]))
+      expect(Dealership.all()).to(eq([test_dealership]))
     end
   end
 
+
+  describe('#cars') do
+    it('creates a car object ') do
+      test_dealership = Dealership.new('bobs dealership')
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+      it("adds a dealership to the array of saved dealerships") do
+        test_dealership = Dealership.new("Bob's Used Cars")
+        test_dealership.save()
+        expect(Dealership.all()).to(eq([test_dealership]))
+      end
+    end
+
+    describe(".all") do
+      it("is empty at first") do
+        expect(Dealership.all()).to(eq([]))
+      end
+    end
+
+    describe(".clear") do
+      it("empties out all of the saved dealerships") do
+        Dealership.new("Bob's Used Cars").save()
+        Dealership.clear()
+        expect(Dealership.all()).to(eq([]))
+      end
+    end
+
+    describe(".find") do
+      it("returns a dealership by its id number") do
+        test_dealership = Dealership.new("Bob's Used Cars")
+        test_dealership.save()
+        test_dealership2 = Dealership.new("Jane's Cars")
+        test_dealership2.save()
+        expect(Dealership.find(test_dealership.dealer_id())).to(eq(test_dealership))
+      end
+    end
 
 end
