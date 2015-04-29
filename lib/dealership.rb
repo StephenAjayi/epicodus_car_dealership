73711d1278786ef177fa6a0bb6ac8279f8521a3b
Paul Ogawa & stephen <student@epicodus.com>
2015-04-29 10:35:46 -0700

    it adds test for dealer_id and save all tests passing

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,9 +1,27 @@
 class Dealership
-  define_method(:initialize) do |name|
-  @name = name
-end
 
-define_method(:name) do
-  @name
+  @@dealerships=[]
+
+  define_method(:initialize) do |name|
+    @name = name
+    @dealer_id = @@dealerships.length+1
   end
+
+  define_method(:name) do
+    @name
+  end
+
+  def dealer_id
+    @dealer_id
+  end
+
+  def save
+    @@dealerships<<self
+  end
+
+
+
+
+
+
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -4,11 +4,28 @@ require "pry"
 
 
 describe(Dealership) do
- 
+
   describe('#name') do
-  it("will create an instance of dealership that has a name") do
-    test_dealership = Dealership.new('Paul and Stephens used car capitol')
-    expect(test_dealership.name()).to(eq('Paul and Stephens used car capitol'))
+    it("will create an instance of dealership that has a name") do
+      test_dealership = Dealership.new('Paul and Stephens used car capitol')
+      expect(test_dealership.name()).to(eq('Paul and Stephens used car capitol'))
+    end
   end
-end
+
+  describe("#dealer_id") do
+    it("will ascribes an id to a dealership") do
+      test_dealership = Dealership.new('bobs dealership')
+      expect(test_dealership.dealer_id()).to(eq(1))
+    end
+  end
+
+  desscribe('#save') do
+    it('saves the object and adds it to an array') do
+      test_dealership = Dealership.new('bobs dealership')
+      test_dealership.save
+      expect(test_dealership.save()).to(eq([test_dealership]))
+    end
+  end
+
+
 end
